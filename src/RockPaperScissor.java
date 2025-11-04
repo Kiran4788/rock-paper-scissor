fdaf181e44d9ce5c5ff5e17df6dfca5cee434c42
kiran4788 <kiranmai.mamidala@gmail.com>
2025-11-04 10:28:59 +0000

    Remove comments

--- src/RockPaperScissor.java
+++ src/RockPaperScissor.java
@@ -1,5 +1,4 @@
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 //This is a simple Rock Paper Scissor game in Java
 import java.util.Random;
 import java.util.Scanner;
